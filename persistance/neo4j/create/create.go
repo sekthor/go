b301cb905549220ca0dbd5fc0f43fe40d5afa25d
sekthor <sekthor@protonmail.ch>
2024-05-29 12:36:14 +0200

    feat: add create statement

---
+++ persistance/neo4j/create/create.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func main() {
+	ctx := context.Background()
+	// URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+	dbUri := os.Getenv("DB_URI")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	driver, err := neo4j.NewDriverWithContext(
+		dbUri,
+		neo4j.BasicAuth(dbUser, dbPassword, ""))
+
+	if err != nil {
+		log.Fatalf("could not connect to database: %v", err)
+	}
+
+	defer driver.Close(ctx)
+
+	err = driver.VerifyConnectivity(ctx)
+	if err != nil {
+		log.Fatalf("could not verify connection: %v", err)
+	}
+
+	log.Printf("successfully connected to %s", dbUri)
+
+	_, _ = neo4j.ExecuteQuery(ctx, driver,
+		"CREATE (r:Recipe {name: $name})",
+		map[string]any{
+			"name": "Lentil Bolognese",
+		}, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase("neo4j"),
+	)
+
+}
